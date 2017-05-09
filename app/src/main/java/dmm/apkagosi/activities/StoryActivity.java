bf01638dab0588762f136b9888f90733814ab9b9
DeTheTurquoise <dominikamatyjewiczmaciejewicz@gmail.com>
2017-05-09 22:35:53 +0200

    Refactoring part 2

---
+++ app/src/main/java/dmm/apkagosi/activities/StoryActivity.java
@@ -0,0 +1,44 @@
+package dmm.apkagosi.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import dmm.apkagosi.R;
+
+/**
+ * Created by ddabrowa on 2017-02-26.
+ */
+
+public class StoryActivity extends GeneralActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.story_screen);
+        displayStoryScreen();
+    }
+
+
+    public void previousStoryPage(View view) {
+        setPreviousPage();
+        displayStoryScreen();
+    }
+
+    public void nextStoryPage(View view) {
+        setNextPage();
+        displayStoryScreen();
+    }
+
+    private void displayStoryScreen() {
+        setPageCounter(screenInfo.getStoryPageLimit());
+        TextView storyText = (TextView) findViewById(R.id.fluff_text);
+        ImageView storyImage = (ImageView) findViewById(R.id.fluff_image);
+
+        storyText.setText(displayTextOnTextView(screenInfo.getStoryPrefix(),"t"));
+        storyImage.setImageResource(displayImageOnImageView(screenInfo.getStoryPrefix(), "i"));
+
+        setPreviousAndNextButton();
+    }
+}
